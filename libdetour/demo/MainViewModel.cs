09624b57a4972641f63fe21c74253f25fed68708
dhj@yushen.com <dhj@yushen.com>
2017-10-26 18:18:09 +0800

    + demo

--- libdetour/demo/MainViewModel.cs
+++ libdetour/demo/MainViewModel.cs
@@ -13,16 +13,30 @@ namespace demo
     using Transform3D = System.Windows.Media.Media3D.Transform3D;
     using TranslateTransform3D = System.Windows.Media.Media3D.TranslateTransform3D;
     using Vector3D = System.Windows.Media.Media3D.Vector3D;
+    using Matrix = SharpDX.Matrix;
+    using Media3D = System.Windows.Media.Media3D;
+    using System.Windows.Media.Animation;
     public class MainViewModel
     {
-        public Viewport3DX modelView { get;  set; }
-        public HelixToolkit.Wpf.SharpDX.Camera Camera { get;  set; }
-        public LineGeometry3D Grid { get;  set; }
+        private bool isAnimated;
+
+        public Viewport3DX modelView { get; set; }
+        public HelixToolkit.Wpf.SharpDX.Camera Camera { get; set; }
+        public PhongMaterial RedMaterial { get; private set; }
+        public PhongMaterial GreenMaterial { get; private set; }
+        public PhongMaterial BlueMaterial { get; private set; }
+        public LineGeometry3D Grid { get; set; }
 
         public Color GridColor { get; set; }
         public TranslateTransform3D GridTransform { get; set; }
-        public Element3DCollection GroundGeometry { get; set; }
-        public Element3DCollection PlayersGeometry { get; set; }
+        //public Element3DCollection GroundGeometry { get; set; }
+        //public Element3DCollection PlayersGeometry { get; set; }
+        public ObservableElement3DCollection GroundGeometry { get; set; }
+        public ObservableElement3DCollection PlayersGeometry { get; set; }
+
+        public DefaultRenderTechniquesManager RenderTechniquesManager { get; private set; }
+        public RenderTechnique RenderTechnique { get; private set; }
+        public DefaultEffectsManager EffectsManager { get; private set; }
         public LineGeometry3D LineGeometry { get; set; }
         public double LineThickness { get; set; }
         public double TriangulationThickness { get; set; }
@@ -31,6 +45,8 @@ namespace demo
         public Color4 AmbientLightColor { get; set; }
         public Color DirectionalLightColor { get; set; }
         public Vector3 DirectionalLightDirection { get; set; }
+        public object LightDirectionTransform { get; private set; }
+        public Vector2 ShadowMapResolution { get; private set; }
         public TranslateTransform3D ModelTransform { get; set; }
         public TranslateTransform3D ModelLineTransform { get; set; }
         public PhongMaterial Material { get; set; }
@@ -47,7 +63,7 @@ namespace demo
             this.PointCount = 1000;
 
             // Lighting Setup
-            this.AmbientLightColor = new Color4(.1f, .1f, .1f, 1.0f);
+            this.AmbientLightColor = new Color4(1f, 1f, 1f, 1.0f);
             this.DirectionalLightColor = Color.White;
             this.DirectionalLightDirection = new Vector3(0, -1, 0);
 
@@ -65,8 +81,90 @@ namespace demo
             this.GridTransform = new TranslateTransform3D(0, -0.01, 0);
 
 
-            this.GroundGeometry = new Element3DCollection();
-            this.PlayersGeometry = new Element3DCollection();
+            //this.GroundGeometry = new Element3DCollection();
+            //this.PlayersGeometry = new Element3DCollection();
+
+
+            RenderTechniquesManager = new DefaultRenderTechniquesManager();
+            RenderTechnique = RenderTechniquesManager.RenderTechniques[DefaultRenderTechniqueNames.Blinn];
+            EffectsManager = new DefaultEffectsManager(RenderTechniquesManager);
+
+
+
+            // setup lighting            
+
+            this.LightDirectionTransform = CreateAnimatedTransform(-DirectionalLightDirection.ToVector3D(), new Vector3D(0, 1, -1), 24);
+            this.ShadowMapResolution = new Vector2(2048, 2048);
+
+            // camera setup
+            this.Camera = new PerspectiveCamera { Position = (Point3D)(-DirectionalLightDirection.ToVector3D()), LookDirection = DirectionalLightDirection.ToVector3D(), UpDirection = new Vector3D(0, 1, 0) };
+
+            // floor plane grid
+            //Grid = LineBuilder.GenerateGrid();
+            //GridColor = SharpDX.Color.Black;
+            //GridTransform = new Media3D.TranslateTransform3D(-5, -1, -5);
+
+            // scene model3d
+            //var b1 = new MeshBuilder();
+            //b1.AddSphere(new Vector3(0, 0, 0), 0.5);
+            //b1.AddBox(new Vector3(0, 0, 0), 1, 0.25, 2, BoxFaces.All);
+            //Model = b1.ToMeshGeometry3D();
+            ////Instances = new[] { Matrix.Translation(0, 0, -1.5f), Matrix.Translation(0, 0, 1.5f) };
+
+            //var b2 = new MeshBuilder();
+            //b2.AddBox(new Vector3(0, 0, 0), 10, 0, 10, BoxFaces.PositiveY);
+            //Plane = b2.ToMeshGeometry3D();
+            //PlaneTransform = new Media3D.TranslateTransform3D(-0, -2, -0);
+            //GrayMaterial = PhongMaterials.LightGray;
+            ////GrayMaterial.TextureMap = new BitmapImage(new System.Uri(@"TextureCheckerboard2.jpg", System.UriKind.RelativeOrAbsolute)); 
+
+            //// lines model3d            
+            //Lines = LineBuilder.GenerateBoundingBox(Model);
+
+            //// model trafos
+            //Model1Transform = new Media3D.TranslateTransform3D(0, 0, 0);
+            //Model2Transform = new Media3D.TranslateTransform3D(-2, 0, 0);
+            //Model3Transform = new Media3D.TranslateTransform3D(+2, 0, 0);
+
+            // model materials
+            RedMaterial = PhongMaterials.Glass;
+            GreenMaterial = PhongMaterials.Green;
+            BlueMaterial = PhongMaterials.Blue;
+
+        }
+        private System.Windows.Media.Media3D.Transform3D CreateAnimatedTransform(Vector3D translate, Vector3D axis, double speed = 4)
+        {
+            var lightTrafo = new Media3D.Transform3DGroup();
+            lightTrafo.Children.Add(new Media3D.TranslateTransform3D(translate));
+
+            var rotateAnimation = new Rotation3DAnimation
+            {
+                RepeatBehavior = RepeatBehavior.Forever,
+                //By = new Media3D.AxisAngleRotation3D(axis, 180),
+                From = new Media3D.AxisAngleRotation3D(axis, 135),
+                To = new Media3D.AxisAngleRotation3D(axis, 225),
+                AutoReverse = true,
+                Duration = TimeSpan.FromSeconds(speed / 4),
+                //IsCumulative = true,                  
+            };
+
+            var rotateTransform = new Media3D.RotateTransform3D();
+            rotateTransform.BeginAnimation(Media3D.RotateTransform3D.RotationProperty, rotateAnimation);
+            lightTrafo.Children.Add(rotateTransform);
+            return lightTrafo;
+        }
+
+        private void OnAnimatedChanged(bool value)
+        {
+            this.isAnimated = value;
+            if (value)
+            {
+                this.LightDirectionTransform = CreateAnimatedTransform(-DirectionalLightDirection.ToVector3D(), new Vector3D(0, 1, -1), 24);
+            }
+            else
+            {
+                this.LightDirectionTransform = System.Windows.Media.Media3D.Transform3D.Identity;
+            }
         }
     }
 }
--- libdetour/demo/MapView.xaml.cs
+++ libdetour/demo/MapView.xaml.cs
@@ -27,23 +27,31 @@ namespace demo
         /// <summary>
         /// The ViewModel
         /// </summary>
-        MainViewModel mViewModel;
-        public MapView()
-        {
-            InitializeComponent();
+        MainViewModel mViewModel=new MainViewModel();
 
-            mViewModel = new MainViewModel();
-            this.DataContext = mViewModel;
-            mViewModel.modelView = viewport3D;
-            mViewModel.Camera = viewport3D.Camera;
+        void SetupViewModel()
+        {
+
+
 
             // Setup the Camera and raise the EventHandler once at the Start
             // X: 24.647, Y: 52.383, Z: 72.829, view direction X: -0.019, Y: -2.411, Z: -2.249
+            mViewModel.modelView = viewport3D;
+            mViewModel.Camera = viewport3D.Camera;
             mViewModel.Camera.Position = new Point3D(24.647, 52.383, 72.829);
             mViewModel.Camera.LookDirection = new Vector3D(-0.019, -2.411, -2.249);
             mViewModel.Grid = LineBuilderExt.GenerateGrid(Vector3.UnitY, -0, 50, -0, 50, 5, 5);
             mViewModel.Camera.Changed += Camera_Changed;
             Camera_Changed(mViewModel.Camera, new EventArgs());
+        }
+        public MapView()
+        {
+            InitializeComponent();
+            SetupViewModel();
+
+            this.DataContext = mViewModel;
+            mViewModel.GroundGeometry = this.groundGroup.Children;
+            mViewModel.PlayersGeometry = this.playersGroup.Children;
         }
 
         void Camera_Changed(object sender, EventArgs e)
@@ -57,30 +65,103 @@ namespace demo
                       String.Format("Z: {0:0.###}", cam.LookDirection.Z);
             // Set the Label Content
             statusLabel.Content = "View from " + pos + ", view direction " + dir;
+            
         }
+
+
+        MeshGeometryModel3D StartBall = null;
+        MeshGeometryModel3D EndBall = null;
+        LineGeometryModel3D WayPath = null;
         /// <summary>
-        /// set start point
+        /// set start point or end point
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void viewport3D_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            var p = viewport3D.FindNearestPoint(e.GetPosition((IInputElement)sender));
+            var point = e.GetPosition((IInputElement)sender);
+            var p = viewport3D.FindNearestPoint(point);
             if (!p.HasValue) return;
+
+
+            if (StartBall == null)
+            {
+                StartBall = CreateBall(PhongMaterials.Green);
+
+            }
+            if (EndBall == null)
+            {
+                EndBall = CreateBall(PhongMaterials.Red);
+
+            }
+            if (WayPath == null)
+            {
+                WayPath = new LineGeometryModel3D();
+                WayPath.Color = SharpDX.Color.Blue;
+                mViewModel.PlayersGeometry.Add(WayPath);
+                WayPath.Attach(mViewModel.modelView.RenderHost);
+            }
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                EndBall.Transform = new TranslateTransform3D(p.Value.X, p.Value.Y , p.Value.Z);
+
+            }
+            else
+            {
+                StartBall.Transform = new TranslateTransform3D(p.Value.X, p.Value.Y , p.Value.Z);
+            }
+            RenderPath();
+
         }
-        /// <summary>
-        /// set end point
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void viewport3D_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+
+        private void RenderPath()
         {
+            LineBuilder lb = new LineBuilder();
+            var startv3 = StartBall.Transform.ToVector3();
+            var endv3 = EndBall.Transform.ToVector3();
 
+            var start = new DHVertex() { x = startv3.X, y = startv3.Y, z = startv3.Z };
+            var end = new DHVertex() { x = endv3.X, y = endv3.Y, z = endv3.Z };
+            DHPath path = new DHPath();
+            if (dh.FindPath(true, start, end, path))
+            {
+                if (path.Length > 1)
+                {
+                    var vs = path.GetPathData();
+                    for (int i = 0; i < path.Length - 1; i++)
+                    {
+
+                        Vector3 v = new Vector3((float)vs[i].x, (float)vs[i].y, (float)vs[i].z);
+                        Vector3 nv = new Vector3((float)vs[i + 1].x, (float)vs[i + 1].y, (float)vs[i + 1].z);
+                        lb.AddLine(new Vector3(v.X, v.Y + 1, v.Z), new Vector3(nv.X, nv.Y + 1, nv.Z));
+                    }
+                }
+                WayPath.Geometry = lb.ToLineGeometry3D();
+
+            }
+            //playersGroup.Children.Clear();
+            //playersGroup.Children.Add(WayPath);
         }
 
+        private MeshGeometryModel3D CreateBall(HelixToolkit.Wpf.SharpDX.Material mat)
+        {
+            MeshGeometryModel3D mgm = new MeshGeometryModel3D();
+            MeshBuilder mb = new MeshBuilder();
+            //mb.AddSphere(MapReader.P2V(p), 2);
+            mb.AddSphere(Vector3.Zero, 0.5);
+            //mb.AddArrow(Vector3.Zero, new Vector3(0, 0, 2), 0.1);
+
+            mgm.Geometry = mb.ToMeshGeometry3D();
+            mgm.Material = mat;
+            mViewModel.PlayersGeometry.Add(mgm);
+
+            mgm.Attach(mViewModel.modelView.RenderHost);
+            return mgm;
+        }
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             lineTriangulatedPolygon.Geometry = mViewModel.LineGeometry;
+
         }
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
@@ -103,9 +184,7 @@ namespace demo
                 DHTriangle[] triangles = dh.GetTriangles();
                 DHVertex[] verts = dh.GetVertexes();
                 var lb = new LineBuilder();
-
-                //mViewModel.GroundGeometry.Clear();
-                //groundGroup.Children.Clear();
+                groundGroup.Children.Clear();
                 mViewModel.ModelTransform = new TranslateTransform3D();
                 for (int i = 0; i < triangles.Length; i++)
                 {
@@ -117,14 +196,12 @@ namespace demo
 
                     MeshGeometryModel3D mgm = MapReader.MakeMeshGeometryModel3DUnity(node, mViewModel.Material);
 
-                    //mgm.HitTest()
-                    //mViewModel.GroundGeometry.Add(mgm);
-                    //groundGroup.Children.Add(mgm);
+                    groundGroup.Children.Add(mgm);
                     mgm.Attach(mViewModel.modelView.RenderHost);
 
                 }
-                
-                lineTriangulatedPolygon.Geometry = lb.ToLineGeometry3D();
+                mViewModel.LineGeometry = lb.ToLineGeometry3D();
+                lineTriangulatedPolygon.Geometry = mViewModel.LineGeometry;
 
             }
 
